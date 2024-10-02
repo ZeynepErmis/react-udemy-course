99b401d4ce1008b04579e7ef0a573a60f8dfb3f4
ZeynepErmis <zzeyneppermis@gmail.com>
2024-10-02 15:07:08 +0300

    03-React-Essentials

---
+++ 03-React-Essentials/01-starting-project/src/components/CoreConcept/CoreConcept.jsx
@@ -0,0 +1,12 @@
+import './CoreConcept.css'
+
+// OBJECT DESTRUCTURING
+export default function CoreConcept({ image, title, description }) {
+    return (
+      <li>
+        <img src={image} alt={title} />
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </li>
+    );
+  }
---
+++ 03-React-Essentials/01-starting-project/src/components/TabButton.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+//This usage is called destructuring { label }. It allows you to extract the label value directly from the props object and use it as just label instead of props.label. This makes the code shorter, more readable, and more organized.
+/* export default function TabButton({ label }) {
+  return (
+    <li>
+      <button>{label}</button>
+    </li>
+  );
+} */
+
+export default function TabButton({ children, onSelect, isSelected }) {
+  console.log("TAB BUTTON COMPONENT EXECUTING");
+
+  return (
+    <li>
+      {/* handleClick(): When you add the parentheses, this function is called immediately. That is, the handleClick function is run immediately when the component is rendered, without waiting for the onClick event to occur. This is usually undesirable, because we don't want the function to run before the event is triggered. */}
+      {/* must not add handleClick(), should be {handleClick} because want to use the function as a value */}
+      <button className={isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button>
+    </li>
+  );
+}
